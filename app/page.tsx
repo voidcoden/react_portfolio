9ebee1954ee5e8097a82831c03d47432910d9b1c
Syafiq2112 <azman.syafiq01@gmail.com>
2024-03-19 20:49:53 +0800

    Create file using next.js

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-bold text-red">Hello World</h1>
+    <h1 className="text-bold text-teal-500">
+      Hello World
+    </h1>
   );
 }
