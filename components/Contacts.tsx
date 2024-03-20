485575afdb83fd2e1cfd75faef8f18bbe4f07438
Syafiq2112 <azman.syafiq01@gmail.com>
2024-03-20 23:08:07 +0800

    Added contacts and projects
    unfinished just took template from the hero and about

--- components/Contacts.tsx
+++ components/Contacts.tsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Contacts = () => {
   return (
-    <div>Contacts</div>
+    <section className="max-container padding-container flex flex-col gap-20 py-10 md:gap-20 xl:py-28 border-2 border-green-500 h-dvh mt-20h-screen justify-center">
+    <div className="relative z-20 flex flex-col border-2 border-green-500">
+        <div className="bg-green-60 relative mt-10 z-10 p-1 shadow-hardShadowRight">
+            <h1 className="text-center bold-32 sm:bold-64 xl:bold-128 ">
+                Get In Touch
+            </h1>
+        </div>
+    </div>
+
+    <div className="relative flex flex-col border-2 border-blue-500">
+        <div className="bg-green-70 relative z-10 pt-4 pb-4 shadow-hardShadowLeft">
+            <p className="bold-20 text-center sm:bold-24 xl:bold-32">
+            azman.syafiq01@gmail.com
+            </p>
+        </div>
+    </div>
+    <div className="relative h-16 w-32">
+        <div className="absolute bottom-0 left-0 h-10 w-16">
+            <Image 
+                src="arrow.svg"
+                alt="arrow"
+                width={40}
+                height={40}
+                className="inline-block cursor-pointer "
+            />
+        </div>
+    </div>
+</section>
   )
 }
 
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,8 +1,55 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <section className="max-container padding-container-small py-10 xl:py-20 md:gap-20 z-20 border-2 bg-black h-dvh mt-20h-screen justify-center">
+    <div>
+      <h1 className='text-white-10 bold-52 sm:bold-128 xl:bold-128 xl:py-10 xl:px-10'>
+        Projects
+      </h1>
+    </div>
+    <div className='flex justify-center items-center'>
+      <div className="max-container flex flex-col xl:flex-row border-2 px-10 border-green-500 ">
+        <div className="relative w-full p-5 border-red-500">
+          <Image 
+            src = "/myphoto.jpg"
+            alt = "myphoto"
+            width={1000}
+            height={1000}
+            className="w-full object-cover sm:w-[500px] sm:h-[500px] sm:mx-auto"
+          />
+        </div>
+        <div className="relative w-full p-5 border-red-500">
+          <Image 
+            src = "/myphoto.jpg"
+            alt = "myphoto"
+            width={1000}
+            height={1000}
+            className="w-full object-cover sm:w-[500px] sm:h-[500px] sm:mx-auto"
+          />
+        </div>
+        <div className="relative w-full p-5 border-red-500">
+          <Image 
+            src = "/myphoto.jpg"
+            alt = "myphoto"
+            width={1000}
+            height={1000}
+            className="w-full object-cover sm:w-[500px] sm:h-[500px] sm:mx-auto"
+          />
+        </div>
+        <div className="relative w-full p-5 border-red-500">
+          <Image 
+            src = "/myphoto.jpg"
+            alt = "myphoto"
+            width={1000}
+            height={1000}
+            className="w-full object-cover sm:w-[500px] sm:h-[500px] sm:mx-auto"
+          />
+        </div>
+      </div>
+    </div>
+  </section>
   )
 }
 
