468299d96b0471390444c62af1e4ecaff85dc121
Syafiq2112 <azman.syafiq01@gmail.com>
2024-03-21 14:09:32 +0800

    project grid layout

--- components/About.tsx
+++ components/About.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <section className="max-container padding-container-small py-10 xl:py-20 md:gap-20 z-20 border-2 bg-black h-dvh mt-20h-screen justify-center">
+    <section className="max-container padding-container-small py-10 xl:py-20 md:gap-20 z-20 border-2 bg-black mt-20h-screen justify-center">
       <div>
         <h1 className='text-white-10 bold-52 sm:bold-128 xl:bold-128 xl:py-10 xl:px-10'>
           About Me
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -3,15 +3,15 @@ import Image from 'next/image'
 
 const Projects = () => {
   return (
-    <section className="max-container padding-container-small py-10 xl:py-20 md:gap-20 z-20 border-2 bg-black h-dvh mt-20h-screen justify-center">
+    <section className="max-container padding-container-small py-10 xl:py-20 md:gap-20 z-20 border-2 bg-black mt-20h-screen justify-center">
     <div>
       <h1 className='text-white-10 bold-52 sm:bold-128 xl:bold-128 xl:py-10 xl:px-10'>
         Projects
       </h1>
     </div>
-    <div className='flex justify-center items-center'>
-      <div className="max-container flex flex-col xl:flex-row border-2 px-10 border-green-500 ">
-        <div className="relative w-full p-5 border-red-500">
+    <div className="flex flex-wrap items-center border-2 border-green-500 justify-center">
+      <div className="m-6 p-0 grid grid-flow-row grid-cols-1 md:grid-cols-2 gap-y-2 md:gap-y-0 md:gap-x-2 border-red-500">
+        <div className="flex-col">
           <Image 
             src = "/myphoto.jpg"
             alt = "myphoto"
@@ -20,7 +20,7 @@ const Projects = () => {
             className="w-full object-cover sm:w-[500px] sm:h-[500px] sm:mx-auto"
           />
         </div>
-        <div className="relative w-full p-5 border-red-500">
+        <div className="flex-col">
           <Image 
             src = "/myphoto.jpg"
             alt = "myphoto"
@@ -29,7 +29,7 @@ const Projects = () => {
             className="w-full object-cover sm:w-[500px] sm:h-[500px] sm:mx-auto"
           />
         </div>
-        <div className="relative w-full p-5 border-red-500">
+        <div className="flex-col">
           <Image 
             src = "/myphoto.jpg"
             alt = "myphoto"
@@ -38,7 +38,7 @@ const Projects = () => {
             className="w-full object-cover sm:w-[500px] sm:h-[500px] sm:mx-auto"
           />
         </div>
-        <div className="relative w-full p-5 border-red-500">
+        <div className="flex-col">
           <Image 
             src = "/myphoto.jpg"
             alt = "myphoto"
